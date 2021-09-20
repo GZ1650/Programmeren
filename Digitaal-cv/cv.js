0bc57245e4637844f1dd5ddc477f1ceabd01ba7a
GZ1650 <165080@student.horizoncollege.nl>
2021-09-20 12:38:18 +0200

    versie 1 van projecte

---
+++ Digitaal-cv/cv.js
@@ -0,0 +1,23 @@
+function home() {
+    window.location.href="cv.html";
+}
+
+function bio() {
+    window.location.href="bio.html";
+}
+
+function info() {
+    window.location.href="info.html";
+}
+
+function skills() {
+    window.location.href="skills.html";
+}
+
+function porto() {
+    window.location.href="porto.html";
+}
+
+function full() {
+    window.location.href="full.html";
+  }
---
+++ Html/eerstejavascript.js
@@ -0,0 +1,5 @@
+// Gino Zwaneveld AEITO21SD-B
+
+function myFunction() {
+  document.getElementsByTagName("P")[0].innerHTML = "Hello World!";
+}
