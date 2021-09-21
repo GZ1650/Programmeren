101d9676ce35a51fe5b7e0f7473dca2fff332ea4
GZ1650 <165080@student.horizoncollege.nl>
2021-09-21 11:34:02 +0200

    Versie 2

---
+++ LoremBV/script.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("btn")
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more";
+            moreText.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Readless"
+            moreText.style.display = "inline";
+        }
+}
